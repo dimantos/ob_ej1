5e9b4e7500128a495c3b1e62c9e86127c448b299
Dmitry Borovskikh <dimantos@me.com>
2022-11-13 14:22:28 +0300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // más comunes
+
+        int var1 = 510;
+        long var2 = 5343434;
+        double var3 = 5.3423256;
+        boolean var4 = true;
+        String var5 = "Dima";
+
+        // todo el resto
+
+        byte var6 = 1;
+        short var7 = 10;
+        Long var8 = null;
+        float var9 = 7.8f;
+        char var10 = 'b';
+
+        System.out.println(var1 + " " + var2 + " " + var3 + " " + var4 + " " + var5 + " " + var6 + " " + var7 + " " + var8 + " " + var9 + " " + var10);
+    }
+}
